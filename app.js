251d5de70e69642c5f4df5635c703b0a6905f3fa
Prathmesh Wavhal <prathmeshwavhal@gmail.com>
2025-03-20 23:16:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("May 06,2022 00:00:00").getTime();
+    const countDate = new Date("May 06,2025 00:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
